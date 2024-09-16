5b4dd4f92c469a54fffba8cbc1ab4b78134030f6
DrueWolf <dwolf0305@gmail.com>
2024-09-15 21:54:02 -0400

    a decent chuink of code

---
+++ src/WeatherForecast.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WeatherForecast = ({ day,img, imgAlt, conditions, time}) => (
+    <div className='weather'>
+        <h2>{day}</h2>
+        <img src={img} alt={imgAlt}/>
+        <p><span>conditions: </span>{conditions}</p>
+        <p><span>time: </span>{time}</p>
+    </div>
+    )
+
+    export default WeatherForecast
